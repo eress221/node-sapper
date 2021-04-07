fd13abe207a0fc3d72e1e2632a38f7b0a0b35729
eress <shno@nexmore.co.kr>
2021-04-07 11:23:14 +0900

    Initial commit

---
+++ src/routes/test.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+    <title>TEST</title>
+</svelte:head>
+
+<script>
+    var test1 = "color:white;";
+
+function f() {
+        alert(document.querySelector("#test").className);
+    }
+</script>
+
+<div id=test class="test" on:click="{f}" style={test1}><span>test</span></div>
+
+<style>
+    div{background-color:red;}
+</style>
